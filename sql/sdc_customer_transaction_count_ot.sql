52b1f0e149bc426c7e936ff9a41d37700c2900c6
eli-10-dev <elijah.gaylan@gmail.com>
2025-12-26 22:31:48 +0800

    Added sdc_customer_transaction_count_ot.sql

---
+++ sql/sdc_customer_transaction_count_ot.sql
@@ -0,0 +1,9 @@
+--CREATE TABLE sdc_customer_transaction_count_ot AS 
+SELECT 
+	"Customer_ID",
+	"Date",
+	COUNT("Transaction_ID") AS customer_transaction_count 
+FROM scanner_data_clean
+GROUP BY "Customer_ID", "Date"
+ORDER BY customer_transaction_count DESC, "Customer_ID"
+;
