b8d8e27e4e3052bcfa9b285a957506c0d9c8e230
eli-10-dev <elijah.gaylan@gmail.com>
2025-12-26 22:39:17 +0800

    Update sdc_customer_transaction_count_ot.sql

--- sql/sdc_customer_transaction_count_ot.sql
+++ sql/sdc_customer_transaction_count_ot.sql
@@ -2,7 +2,8 @@
 SELECT 
 	"Customer_ID",
 	"Date",
-	COUNT("Transaction_ID") AS customer_transaction_count 
+	COUNT("Transaction_ID") AS customer_transaction_count,
+	EXTRACT(MONTH FROM "Date") AS "Month"
 FROM scanner_data_clean
 GROUP BY "Customer_ID", "Date"
 ORDER BY customer_transaction_count DESC, "Customer_ID"
